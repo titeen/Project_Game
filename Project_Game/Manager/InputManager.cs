0d65fb45029936ecc821163bb040fbf8bdb21f0a
Fatima Huseein <fatima.huseein@student.ap.be>
2024-12-26 14:13:45 +0100

    InputManager toegevoeg voor keyboard controle

---
+++ Project_Game/Manager/InputManager.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace Project_Game.Manager
+{
+    public class InputManager
+    {
+        private KeyboardState _currentState;
+        private KeyboardState _previousState;
+
+        // Update de invoerstatus
+        //moet in methode van game worden aangeroepen om huidige en vorige toestand van toetsen bij te houden
+        public void Update()
+        {
+            _previousState = _currentState;
+            _currentState = Keyboard.GetState();
+        }
+
+        // Controleer of een bepaalde toets momenteel ingedrukt is
+        public bool IsKeyDown(Keys key)
+        {
+            return _currentState.IsKeyDown(key);
+        }
+
+        // Controleer of een bepaalde toets net is ingedrukt (één keer)
+        public bool IsKeyPressed(Keys key)
+        {
+            return _currentState.IsKeyDown(key) && !_previousState.IsKeyDown(key);
+        }
+
+        // Controleer of een bepaalde toets is losgelaten
+        public bool IsKeyReleased(Keys key)
+        {
+            return !_currentState.IsKeyDown(key) && _previousState.IsKeyDown(key);
+        }
+    }
+}
--- Project_Game/UI/StartScreen.cs
+++ Project_Game/UI/StartScreen.cs
@@ -7,10 +7,11 @@ namespace Project_Game.UI
 {
     public class StartScreen : Screen
     {
-        private SpriteBatch _spriteBatch;
         private SpriteFont _titleFont;
         private SpriteFont _menuFont;
         private Texture2D _backgroundTexture;
+        private Texture2D _scrollBackgroundTexture;
+
         private string[] options = { "Level 1", "Level 2" };
         private int selectedOption = 0;
 
@@ -18,10 +19,11 @@ namespace Project_Game.UI
 
         public override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(_game.GraphicsDevice);
+            // Laad bronnen
             _titleFont = _game.Content.Load<SpriteFont>("basicFontBig");
             _menuFont = _game.Content.Load<SpriteFont>("basicFont");
             _backgroundTexture = _game.Content.Load<Texture2D>("StartScreen5");
+            _scrollBackgroundTexture = _game.Content.Load<Texture2D>("scroll_background");
         }
 
         public override void Update(GameTime gameTime)
@@ -39,74 +41,65 @@ namespace Project_Game.UI
             if (Keyboard.GetState().IsKeyDown(Keys.Enter))
             {
                 if (selectedOption == 0)
-                {
                     _game.ChangeScreen(new Level1(_game));
-                }
                 else if (selectedOption == 1)
-                {
                     _game.ChangeScreen(new Level2(_game));
-                }
             }
         }
-
 
         public override void Draw(GameTime gameTime)
         {
             _game.GraphicsDevice.Clear(Color.Black);
-            _spriteBatch.Begin();
+            _game.SpriteBatch.Begin();
 
             // Teken de achtergrond
-            _spriteBatch.Draw(_backgroundTexture, _game.GraphicsDevice.Viewport.Bounds, Color.White);
+            _game.SpriteBatch.Draw(_backgroundTexture, _game.GraphicsDevice.Viewport.Bounds, Color.White);
 
+            // Teken de titel
             string title = "Save the Princess";
-            string[] titleWords = title.Split(' ');  // Split de titel in woorden
+            string[] titleWords = title.Split(' ');
             float totalWidth = 0;
 
-            // Meet de totale breedte van de tekst inclusief de spaties tussen woorden
             foreach (string word in titleWords)
             {
-                totalWidth += _titleFont.MeasureString(word).X + 10; // Voeg 10 pixels toe voor tussenruimte tussen de woorden
+                totalWidth += _titleFont.MeasureString(word).X + 10;
             }
 
-            // Centreer de titel
             float titleX = (_game.GraphicsDevice.Viewport.Width - totalWidth) / 2;
-            float titleY = 10; //Afstand van de hoogte
+            float titleY = 10;
 
-            // Teken een achtergrondbox voor de titel
-            float backgroundWidth = totalWidth; 
-            float backgroundHeight = _titleFont.MeasureString(title).Y + 20;  // Voeg wat hoogte toe voor de achtergrond
+            float backgroundWidth = totalWidth;
+            float backgroundHeight = _titleFont.MeasureString(title).Y + 20;
             Rectangle titleBox = new Rectangle((int)titleX - 40, (int)titleY - 10, (int)backgroundWidth + 75, (int)backgroundHeight);
-            _spriteBatch.Draw(_game.Content.Load<Texture2D>("scroll_background"), titleBox, Color.White);
 
-            // Teken de titel met spaties tussen de woorden
+            _game.SpriteBatch.Draw(_scrollBackgroundTexture, titleBox, Color.White);
+
             float currentX = titleX;
             foreach (string word in titleWords)
             {
-                _spriteBatch.DrawString(_titleFont, word, new Vector2(currentX, titleY), Color.Black);
+                _game.SpriteBatch.DrawString(_titleFont, word, new Vector2(currentX, titleY), Color.Black);
                 currentX += _titleFont.MeasureString(word).X + 10;
             }
 
-            // Bereken de breedte van beide opties en plaats ze horizontaal gecentreerd onderaan het scherm
-            float optionsTotalWidth = _menuFont.MeasureString(options[0]).X + _menuFont.MeasureString(options[1]).X + 50; // 50 pixels tussenruimte
+            // Teken menu-opties
+            float optionsTotalWidth = _menuFont.MeasureString(options[0]).X + _menuFont.MeasureString(options[1]).X + 50;
             float optionsStartX = (_game.GraphicsDevice.Viewport.Width - optionsTotalWidth) / 2;
             float optionsY = _game.GraphicsDevice.Viewport.Height - 50;
 
             for (int i = 0; i < options.Length; i++)
             {
                 string option = options[i];
-                Vector2 optionSize = _menuFont.MeasureString(option);  // Meet de breedte van de optie
-                float optionX = optionsStartX + (i * (optionSize.X + 50));  // Plaats opties naast elkaar met 50 pixels tussenruimte
+                Vector2 optionSize = _menuFont.MeasureString(option);
+                float optionX = optionsStartX + (i * (optionSize.X + 50));
 
-                // Voeg een achtergrondbox toe voor de menu-optie
                 Rectangle optionBox = new Rectangle((int)optionX - 15, (int)optionsY - 10, (int)optionSize.X + 30, (int)optionSize.Y + 20);
-                _spriteBatch.Draw(_game.Content.Load<Texture2D>("scroll_background"), optionBox, Color.White);
+                _game.SpriteBatch.Draw(_scrollBackgroundTexture, optionBox, Color.White);
 
-                // Hooglicht de geselecteerde optie en teken de tekst
                 Color optionColor = i == selectedOption ? Color.White : Color.Black;
-                _spriteBatch.DrawString(_menuFont, option, new Vector2(optionX, optionsY), optionColor);
+                _game.SpriteBatch.DrawString(_menuFont, option, new Vector2(optionX, optionsY), optionColor);
             }
 
-            _spriteBatch.End();
+            _game.SpriteBatch.End();
         }
     }
 }
